21642e131db715bdf3dddd0a9c34572e13a1aff2
BaconErie <baconerie0001@gmail.com>
2022-03-11 20:39:39 -0500

    Create config

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# This is the configuration file for ChillBot
+# Change settings for the bot here
+
+#######################
+# Permission Settings #
+#######################
+
+CO_OWNER_ID = 731185273681215650
