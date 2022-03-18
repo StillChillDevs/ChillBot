93fe4cdbc6e6288da7ebc63833c23e355249be62
BaconErie <77812176+BaconErie@users.noreply.github.com>
2022-03-18 13:27:53 -0400

    Foundations

--- src/cogs/can-confirm.py
+++ src/cogs/can-confirm.py
@@ -1,1 +1,11 @@
+# CanConfirm
+# A new member confirmation cog for Discord bots
+# Copyright 2022 StillChillDevs. See the LICENSE file to see full license
 
+import discord
+from discord.ext import commands
+import storage
+
+class CanConfirm(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
