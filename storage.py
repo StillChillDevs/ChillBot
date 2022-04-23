7e7f287a04112d578ba5e973c495dd0bb512b204
BaconErie <baconerie0001@gmail.com>
2022-04-23 18:08:36 -0400

    Complete storage.py

---
+++ storage.py
@@ -0,0 +1,139 @@
+# Database Wrapper
+# Turns Python functions into SQL
+
+import sqlite3
+
+####################
+# GLOBAL USER DATA #
+####################
+
+def get_global_user_data(user_id, key):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT value, type FROM global_user_data WHERE user_id=? AND key=?', (user_id, key))
+
+    output = cursor.fetchone()
+
+    if len(output) == 0:
+        # No data exists
+        connection.close()
+        return None
+
+    value = output[0]
+    value_type = output[1]
+
+    if value_type == 'int':
+        value = int(value)
+    elif value_type == 'bool':
+        value = bool(value)
+    elif value_type == 'str':
+        value = str(value)
+
+    connection.close()
+
+    return value
+
+def set_global_user_data(user_id, key, value):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    value_type = type(value).__name__
+
+    # Delete previous entry
+    cursor.execute('DELETE FROM global_user_data WHERE user_id=? AND key=?', (user_id, key))
+
+    # Add new entry
+    cursor.execute('INSERT INTO global_user_data (user_id, key, value, type) VALUES (?,?,?,?)', (user_id, key, value, value_type))
+
+    connection.close()
+
+###################
+# GUILD USER DATA #
+###################
+
+def get_guild_user_data(guild_id, user_id, key):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT value, type FROM guild_user_data WHERE guild_id=? AND user_id=? AND key=?', (guild_id, user_id, key))
+
+    output = cursor.fetchone()
+
+    if len(output) == 0:
+        # No data exists
+        connection.close()
+        return None
+
+    value = output[0]
+    value_type = output[1]
+
+    if value_type == 'int':
+        value = int(value)
+    elif value_type == 'bool':
+        value = bool(value)
+    elif value_type == 'str':
+        value = str(value)
+
+    connection.close()
+
+    return value
+
+def set_guild_user_data(guild_id, user_id, key, value):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    value_type = type(value).__name__
+
+    # Delete previous entry
+    cursor.execute('DELETE FROM guild_user_data WHERE guild_id=? AND user_id=? AND key=?', (guild_id, user_id, key))
+
+    # Add new entry
+    cursor.execute('INSERT INTO guild_user_data (guild_id, user_id, key, value, type) VALUES (?,?,?,?,?)', (guild_id, user_id, key, value, value_type))
+
+    connection.close()
+
+##############
+# GUILD DATA #
+##############
+
+def get_guild_data(guild_id, key):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT value, type FROM guild_data WHERE guild_id=? AND key=?', (guild_id, key))
+
+    output = cursor.fetchone()
+
+    if len(output) == 0:
+        # No data exists
+        connection.close()
+        return None
+
+    value = output[0]
+    value_type = output[1]
+
+    if value_type == 'int':
+        value = int(value)
+    elif value_type == 'bool':
+        value = bool(value)
+    elif value_type == 'str':
+        value = str(value)
+
+    connection.close()
+
+    return value
+
+def set_guild_data(guild_id, key, value):
+    connection = sqlite3.connect('bot-data.db')
+    cursor = connection.cursor()
+
+    value_type = type(value).__name__
+
+    # Delete previous entry
+    cursor.execute('DELETE FROM guild_data WHERE guild_id=? AND key=?', (guild_id, key))
+
+    # Add new entry
+    cursor.execute('INSERT INTO guild_data (guild_id, key, value, type) VALUES (?,?,?,?)', (guild_id, key, value, value_type))
+
+    connection.close()
