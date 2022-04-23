bb6dc31bb38a23b7c806d8324f4ed3125ae5152f
BaconErie <baconerie0001@gmail.com>
2022-04-23 18:48:32 -0400

    Create and add setup for automod.py

---
+++ src/cogs/automod.py
@@ -0,0 +1,9 @@
+# Automoderation tools for ChillBot
+# Copyright 2022 StillChillDevs. See the LICENSE file to see full license
+
+import discord
+from discord.ext import commands
+import storage
+
+class AutoMod(commands.Cog):
+    pass
