34e119c080dddd62a2fec23cc2e6908bafddb163
BaconErie <77812176+BaconErie@users.noreply.github.com>
2022-05-05 13:29:17 -0400

    Finish spam ping code wooo

--- src/cogs/automod.py
+++ src/cogs/automod.py
@@ -22,32 +22,7 @@ class AutoMod(commands.Cog):
     # PROCESS FUNCTIONS #
     #####################
 
-    def check_spam_ping(message):
-        # Does the server even have spam ping detection on? If no, return
-        # Does the message have mentions? If no, return
-        # Get guild.user.last_pinged
-        # Does mentioned user_id exist in guild.user.last_pinged.keys()?
-        #   Yes:
-        #   Is guild.user.last_pinged[mentioned user_id]['last_pinged'] - current_unix_time < 60?
-        #       Yes:
-        #       Change guild.user.last_pinged[mentioned user_id]['ping_count'] by 1
-        #       
-        #       No:
-        #       Set guild.user.last_pinged[mentioned user_id]['ping_count'] to 1 
-        #
-        #   No:
-        #   Set guild.user.last_pinged[mentioned user_id] to {}
-        #   Set guild.user.last_pinged[mentioned user_id]['last_pinged'] to current unix time
-        #   Set guild.user.last_pinged[mentioned user_id]['ping_count'] to 1
-        # 
-        # Get guild.user.last_pinged[mentioned user_id]['ping_count']
-        # Get guild.ping_per_second_max
-        # Is guild.user.last_pinged[mentioned user_id]['ping_count']
-        #   Yes:
-        #   Get guild.spam_ping_punishment
-        #   punish(guild_id, member, punishment_type)
-
-    
+    def check_spam_ping(message):    
         # Does the server even have spam ping detection on? If no, return
         # Does the message have mentions? If no, return
         if not storage.get_guild_data(message.guild.id, 'is_spam_ping_detect') or len(message.mentions) == 0:
@@ -86,6 +61,9 @@ class AutoMod(commands.Cog):
 
                 # Set guild.user.last_pinged[mentioned user_id]['ping_count'] to 1
                 last_pinged[mentioned_user.id]['ping_count'] = 1
+
+                # Store the updated last_pinged
+                storage.set_guild_user_data(message.guild.id, message.author.id, 'last_pinged', last_pinged)
             
             elif last_pinged != None and not (mentioned_user in last_pinged.keys()):
                 # Set guild.user.last_pinged[mentioned user_id] to {}
@@ -96,3 +74,20 @@ class AutoMod(commands.Cog):
 
                 # Set guild.user.last_pinged[mentioned user_id]['ping_count'] to 1
                 last_pinged[mentioned_user.id]['ping_count'] = 1
+
+                # Store the updated last_pinged
+                storage.set_guild_user_data(message.guild.id, message.author.id, 'last_pinged', last_pinged)
+            
+
+            # PUNISHMENT CODE #
+
+            # Get guild.user.last_pinged[mentioned user_id]['ping_count']
+            ping_count = storage.get_guild_user_data(message.guild.id, message.author.id, 'last_pinged')[mentioned_user.id]['ping_count']
+
+            # Get guild.ping_per_second_max
+            ping_per_second_max = storage.get_guild_data(message.guild.id, 'ping_per_second_max')
+            
+            # Is guild.user.last_pinged[mentioned user_id]['ping_count'] > ping_per_second_max
+            if ping_count > ping_per_second_max:
+                spam_ping_punishment = storage.get_guild_data(message.guild.id, 'spam_ping_punishment')
+                punish(spam_ping_punishment)
