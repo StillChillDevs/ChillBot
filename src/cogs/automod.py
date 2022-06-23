b0afe46685e3b9e1b94cce4a83965b29317c0595
BaconErie <baconerie0001@gmail.com>
2022-06-23 14:05:14 -0400

    Being goal 2

--- src/cogs/automod.py
+++ src/cogs/automod.py
@@ -1,10 +1,12 @@
 # Automoderation tools for ChillBot
 # Copyright 2022 StillChillDevs. See the LICENSE file to see full license
 
-import discord
-from discord.ext import commands
-import storage
 from datetime import datetime, timezone
+
+import discord
+import storage
+from discord.ext import commands
+
 
 class AutoMod(commands.Cog):
     def __init__(self, bot):
@@ -35,4 +37,16 @@ class AutoMod(commands.Cog):
         if not storage.get_guild_data(message.guild.id, 'is_spam_ping_detect') or len(message.mentions) == 0:
             return
 
+        # Goal 1: Check how many members were mentioned by this message, set that to members_mentioned
+        members_mentioned = len(message.mentions)
+
+        # Goal 2: Increment the amount of times the sender mentioned others in the last 60 seconds
+        
+        # First we have to get the mention_times list
+        # mention_times contains the times when the member mentioned someone
+        # For example, if the member mentioned 3 people at time x, then there will be 3 entries of x in mention_times
+        # The time is in unix time in seconds
+        # A mention_times is made for each user
+
+        
         
