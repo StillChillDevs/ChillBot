9c0805fcd433f3f12334d94fe0ddd0fe4c123a96
BaconErie <baconerie0001@gmail.com>
2022-06-23 14:43:08 -0400

    Rewrite check spam ping function

--- src/cogs/automod.py
+++ src/cogs/automod.py
@@ -46,7 +46,52 @@ class AutoMod(commands.Cog):
         # mention_times contains the times when the member mentioned someone
         # For example, if the member mentioned 3 people at time x, then there will be 3 entries of x in mention_times
         # The time is in unix time in seconds
-        # A mention_times is made for each user
+        # A mention_times is made for each user for each guild
 
+        mention_times = storage.get_guild_user_data(message.guild.id, message.author.id, 'mention_times')
+
+        # If the mention_times doesn't exist, make one and set it to blank for now
+        if mention_times == None:
+            storage.set_guild_user_data(message.guild.id, message.author.id, 'mention_times', [])
+            mention_times = storage.get_guild_user_data(message.guild.id, message.author.id, 'mention_times')
         
+        # Get the UTC unix time when the message was sent in seconds, then add the time to mention times
+        # ... based on how many members was mentioned (members_mentioned)
+
+        message_create_time = message.created_at.timestamp()
+
+        print('Line 63, this is unix time at which message was created' + str(message_create_time))
+        for x in range(members_mentioned):
+            mention_times.append(message_create_time)
         
+        # Goal 3: Check if the the sender mentioned people above the limit, if so mute them
+        # First we need to get the mention_limit
+        mention_limit = storage.get_guild_data(message.guild.id, 'mention_limit')
+
+        if mention_limit == None:
+            # Something is clearly wrong, for now just print to output
+            print('Line 74, mention_limit does not exist, even though spam ping detection is on')
+
+        mentions_in_last_minute = 0
+
+        # Loop through mentions_times, and see which times are less than 60 seconds
+        for time in mention_times:
+            current_time = datetime.utcnow().timestamp()
+            
+            if current_time - time <= 60:
+                # Mentioned in the last minute, increment mentions_in_last_minute
+                mentions_in_last_minute += 1
+            else:
+                # Otherwise we can actually just remove the time from mention_times
+                mention_times.remove(time)
+        
+        # Now we check if the mentions_in_last_minute is more than mention_limit
+        if mentions_in_last_minute > mention_limit:
+            # Mentions more than minute, get mute duration and mute the user
+            mute_duration = storage.get_guild_data(message.guild.id, 'spam_ping_mute_duration')
+
+            if mute_duration == None:
+                # Something is clearly wrong, for now just print to output
+                print('Line 95, mute_duration does not exist, even though spam ping detection is on')
+            
+            mute(message.guild.id, message.member.id, f'Mention spam: Member reached mentions per minute limit of {mention_limit}')
