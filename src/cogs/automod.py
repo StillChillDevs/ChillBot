f85be29464c516dea6067b66fb402fbbf38e697f
BaconErie <baconerie0001@gmail.com>
2022-07-02 19:15:27 -0400

    Do a bunch of stuff

--- src/cogs/automod.py
+++ src/cogs/automod.py
@@ -4,27 +4,71 @@
 from datetime import datetime, timezone
 
 import discord
-import storage
 from discord.ext import commands
+from discord import Option, Embed, Forbidden
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import storage
 
 
 class AutoMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        # Create command groups
+        self.pingspam = self.bot.create_group('pingspam', 'Automod for ping spam')
     
-    ###################
-    # EVENT LISTENERS #
-    ###################
+    ######################################
+    # EVENT LISTENERS AND SLASH COMMANDS #
+    ######################################
 
     @commands.Cog.listener()
-    async def on_message(message):
-        pass
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+
+        await self.check_spam_ping(message)
+
+    # Muterole command
+    @commands.slash_command(guild_ids=[976313937429684254])
+    async def muterole(self, ctx,
+        muterole: Option(discord.Role, 'The role to give to a muted person'),
+        description='Set the server mute role'
+    ):
+        # Get the id of the muterole
+        # Set that with the key 'muterole' in the guild storage
+        # Confirm with the user that we have set the mute role
+        # WE TRUST THAT DISCORD GAVE US A VALID ROLE
+
+        storage.set_guild_data(ctx.guild.id, 'muterole', muterole.id)
+
+        # Confirm with the user
+        # Make the embed then send
+        embed = Embed(color=0x05ff00)
+        embed.add_field(name='Muterole Set', value=f'Successfully set {muterole.mention} as the muterole', inline=False)
+        embed.set_footer(text='Make sure that the muterole actually works!')
+        ctx.respond(embed=embed)
+    
+    # Mute command
+    @commands.slash_command(guild_ids=[976313937429684254])
+
+
+    # Ping Spam Slash Commands
+    @commands.slash_command(guild_ids=[976313937429684254])
+    async def set(self, ctx,
+        limit: Option(int, 'The max amount of mentions per minute'),
+        mute_duration: Option(int, 'Mute duration, IN MINUTES, if the person is muted for spam pinging'),
+        pingspam = self.bot.create_group()
+    ):
+        self.pingspam
 
     #####################
     # PROCESS FUNCTIONS #
     #####################
 
-    def check_spam_ping(message):  
+    async def check_spam_ping(self, message):  
         '''Takes in the message and checks if the user is spam pinging'''
 
         # Goals:
@@ -94,4 +138,59 @@ class AutoMod(commands.Cog):
                 # Something is clearly wrong, for now just print to output
                 print('Line 95, mute_duration does not exist, even though spam ping detection is on')
             
-            mute(message.guild.id, message.member.id, f'Mention spam: Member reached mentions per minute limit of {mention_limit}')
+            await self.mute(message.guild.id, message.member.id, self.bot.id, mute_duration, f'Mention spam: Member reached mentions per minute limit of {mention_limit}')
+        
+    async def mute(self, guild_id, member_id, moderator_id, duration, reason):
+        '''Mutes a member in a given server for the specified amount of minutes, or forever if none'''
+        
+        # STEPS
+        # 1. Check to see if the mute role is set, if not alert and return
+        # 2. Check to make sure the mute role exists, if not alert and return
+        # 3. Check to make sure the member exists, if not alert and return
+        # 4. Check to make sure the moderator has manage role permission, if not alert and return
+        # 5. Make sure duration is an integer, if not alert and return
+        # 6. Try to add the role to the member, if there is a permission error alert and return
+        # 7. If we are able to add the muted role, record it in the guild user storage
+
+        muterole_id = storage.get_guild_data(guild_id, 'muterole')
+        guild = self.bot.get_guild(guild_id)
+        member = guild.get_member(member_id)
+        moderator = guild.get_member(moderator_id)
+
+        # Check if mute role is set
+        if muterole_id == None:
+            # If not return
+            return 'mute role not set'
+        
+        muterole = guild.get_role(muterole_id)
+        
+        # Check if role exists
+        if muterole == None:
+            # If not return
+            return 'mute role does not exist'
+        
+        # Check if the member exists
+        if member == None:
+            # If not return
+            return 'member does not exist'
+
+        # Check if the moderator has the manage role permission
+        if not moderator.guild_permissions.manage_roles:
+            # If no manage roles perm return
+            return 'moderator does not have perms'
+        
+        # Make sure duration is an integer
+        if type(duration) != int:
+            return 'duration not an integer'
+        
+        # Try to add the role to the member
+        try:
+            await member.add_roles(muterole)
+        except Forbidden:
+            return 'no perms'
+        
+        # Record that the user is muted in the storage
+        storage.set_guild_user_data(guild_id, member_id, 'is_muted', True)
+
+        # Return with success
+        return 'success'
